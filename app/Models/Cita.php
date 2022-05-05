abde58e18ce24d865bcdf91e1f109496b217c04a
MigArv96 <84158783+MigArv96@users.noreply.github.com>
2022-05-05 11:09:40 -0600

    Se añadieron todas las colecciones a la DB, se crearon los modelos y los controladores

---
+++ app/Models/Cita.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model;
+
+class Cita extends Model
+{
+    protected $connection = 'mongodb';
+    protected $collection = 'Cita';
+}
---
+++ database/migrations/2022_05_04_024226_create_pacientes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pacientes', function (Blueprint $table) {
+            $table->nombre();
+            $table->apellido();
+            $table->edad();
+            $table->fNac();
+            $table->direccion();
+            $table->telefono();
+            $table->dui();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pacientes');
+    }
+};
