d35578e8d72c3f29d01bedb7fc86bbddc008e764
Jaime Portillo <jaime.portillo@catolica.edu.sv>
2022-05-13 14:37:57 -0600

    ultimos controladores

--- app/Http/Controllers/SubgrupoCieController.php
+++ app/Http/Controllers/SubgrupoCieController.php
@@ -1,10 +1,70 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\subgrupo_cie;
 use Illuminate\Http\Request;
 
 class SubgrupoCieController extends Controller
 {
-    //
+ /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $datasubgrupo=subgrupo_cie::all();
+        return view('subgrupo_cie.index', compact('datasubgrupo'));
+
+    }
+     /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('subgrupo.create');
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $datasubgrupo = $request->except('_token','saveitem');
+        subgrupo_cie::insert($datasubgrupo);
+        //return response()->json($dataProducts);
+        return redirect('subgrupo_cie');
+
+    }
+    public function edit($id) {
+        $datasubgrupo = subgrupo_cie::find($id);
+        return view('subgrupo.edit', compact('datasubgrupo'));
+     }
+
+    public function update(Request $request, $id){
+
+        $datasubgrupo = subgrupo_cie::find($id);
+        $datasubgrupo->update($request->all());
+        $datasubgrupo->save();
+        return redirect('subgrupo_cie');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\subgrupo_cie  $subgrupo_cie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy ($id)
+    {
+
+        $datasubgrupo = subgrupo_cie::find($id);
+        $datasubgrupo->delete();
+        return redirect('subgrupo_cie');
+    }
 }
--- app/Http/Controllers/VademecumController.php
+++ app/Http/Controllers/VademecumController.php
@@ -1,10 +1,83 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\vademecum;
 use Illuminate\Http\Request;
 
 class VademecumController extends Controller
 {
-    //
+   /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $datavademecum=vademecum::all();
+        return view('vademecum.index', compact('vademecum'));
+
+    }
+     /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('vademecum.create');
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $datavademecum = $request->except('_token','saveitem');
+        vademecum::insert($datavademecum);
+        //return response()->json($dataProducts);
+        return redirect('vademecum');
+
+        /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    }
+    public function edit($id) {
+        $datavademecum = vademecum::firstOrFail($id);
+        return view('vademecum.edit', compact('datavademecum'));
+     }
+
+     /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id){
+
+        $datavademecum = vademecum::firstOrFail($id);
+        $datavademecum->update($request->all());
+        $datavademecum->save();
+        return redirect('vademecum');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\vademecum  $vademecum
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy ($id)
+    {
+
+        $datavademecum = vademecum::find($id);
+        $datavademecum->delete();
+        return redirect('vademecum');
+    }
 }
