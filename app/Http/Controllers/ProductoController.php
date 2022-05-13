dece7677fc56075343ec396f8246c8a3a87eb1fa
Jaime Portillo <jaime.portillo@catolica.edu.sv>
2022-05-13 14:03:21 -0600

    controller prod/rece

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -1,10 +1,71 @@
 <?php
-
+use App\Models\Producto;
 namespace App\Http\Controllers;
 
+use App\Models\Producto;
 use Illuminate\Http\Request;
 
 class ProductoController extends Controller
 {
-    //
+   /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $dataproducto=Producto::all();
+        return view('producto.index', compact('datapoducto'));
+
+    }
+     /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('producto.create');
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $dataproducto = $request->except('_token','saveitem');
+        Producto::insert($dataproducto);
+        //return response()->json($dataProducts);
+        return redirect('producto');
+
+    }
+    public function edit($id) {
+        $dataproducto = Producto::find($id);
+        return view('producto.edit', compact('dataproducto'));
+     }
+
+    public function update(Request $request, $id){
+
+        $dataproducto = Producto::find($id);
+        $dataproducto->update($request->all());
+        $dataproducto->save();
+        return redirect('producto');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Producto  $producto
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy ($id)
+    {
+
+        $dataproducto = Producto::find($id);
+        $dataproducto->delete();
+        return redirect('producto');
+    }
 }
--- app/Http/Controllers/RecetaController.php
+++ app/Http/Controllers/RecetaController.php
@@ -1,10 +1,70 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Receta;
 use Illuminate\Http\Request;
 
 class RecetaController extends Controller
 {
-    //
+   /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $datareceta=Receta::all();
+        return view('receta.index', compact('datareceta'));
+
+    }
+     /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('receta.create');
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $datareceta = $request->except('_token','saveitem');
+        Receta::insert($datareceta);
+        //return response()->json($dataProducts);
+        return redirect('receta');
+
+    }
+    public function edit($id) {
+        $datareceta = Receta::find($id);
+        return view('receta.edit', compact('datareceta'));
+     }
+
+    public function update(Request $request, $id){
+
+        $datareceta = Receta::find($id);
+        $datareceta->update($request->all());
+        $datareceta->save();
+        return redirect('receta');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Receta  $receta
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy ($id)
+    {
+
+        $datareceta = Receta::find($id);
+        $datareceta->delete();
+        return redirect('receta');
+    }
 }
